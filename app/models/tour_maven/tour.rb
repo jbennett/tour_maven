59fda556c7c29862f5abd08f2b78cb15167e2847
Jonathan Bennett <jonathan@jbennett.me>
2023-07-30 15:09:43 -0400

    avoid blank fields

--- app/models/tour_maven/tour.rb
+++ app/models/tour_maven/tour.rb
@@ -2,7 +2,15 @@ module TourMaven
   class Tour < ApplicationRecord
 
     scope :in_path, ->(path) {
-      where('page_filter IS NULL or page_filter = ?', path)
+      where("page_filter IS NULL OR page_filter = ?", path)
     }
+
+    def page_filter=(value)
+      super(value.presence)
+    end
+
+    def content_selector=(value)
+      super(value.presence)
+    end
   end
 end
