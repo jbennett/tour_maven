bc19b11ddc98baa167805a19fd9aec60dc0d1bae
Jonathan Bennett <jonathan@jbennett.me>
2023-07-30 14:48:25 -0400

    extract default class

--- app/helpers/tour_maven/shared_helpers.rb
+++ app/helpers/tour_maven/shared_helpers.rb
@@ -1,10 +1,13 @@
 module TourMaven
   module SharedHelpers
     def tours_controller(tour_selector: '.tour_outlet')
+    DEFAULT_TOUR_OUTLET_CLASS = "tour_outlet"
+    def tours_controller(tour_class: DEFAULT_TOUR_OUTLET_CLASS)
+      tour_selector = ".#{tour_class}" # the outlet uses a selector not the class directly
       tag.template nil, data: { controller: 'tours', tours_tour_outlet: tour_selector}
     end
 
-    def tour_controller(id, user: nil, tour_class: 'tour_outlet')
+    def tour_controller(id, user: nil, tour_class: DEFAULT_TOUR_OUTLET_CLASS)
       tour = TourMaven::Tour.find(id)
 
       tag.template tour.configuration, data: { controller: 'tour', tour_id_value: id }, class: tour_class
