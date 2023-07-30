4d070ff9e72c840e545a323e53b22cf3dc120dae
Jonathan Bennett <jonathan@jbennett.me>
2023-07-30 14:49:47 -0400

    add helper to insert all available tours

--- app/helpers/tour_maven/shared_helpers.rb
+++ app/helpers/tour_maven/shared_helpers.rb
@@ -1,7 +1,16 @@
 module TourMaven
   module SharedHelpers
-    def tours_controller(tour_selector: '.tour_outlet')
     DEFAULT_TOUR_OUTLET_CLASS = "tour_outlet"
+
+    def available_tour_controllers(tour_class: DEFAULT_TOUR_OUTLET_CLASS)
+      path = request.path
+      tours = TourMaven::Tour.in_path(path)
+
+      tours.each do |tour|
+        concat tour_controller(tour.id, tour_class: tour_class)
+      end
+    end
+
     def tours_controller(tour_class: DEFAULT_TOUR_OUTLET_CLASS)
       tour_selector = ".#{tour_class}" # the outlet uses a selector not the class directly
       tag.template nil, data: { controller: 'tours', tours_tour_outlet: tour_selector}
