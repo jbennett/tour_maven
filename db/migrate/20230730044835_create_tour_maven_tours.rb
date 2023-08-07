0e0e609f2e05ff62ca6f472c90c589da63f1bf2b
Jonathan Bennett <jonathan@jbennett.me>
2023-08-07 06:12:51 -0400

    fix for sqlite

--- db/migrate/20230730044835_create_tour_maven_tours.rb
+++ db/migrate/20230730044835_create_tour_maven_tours.rb
@@ -2,7 +2,12 @@ class CreateTourMavenTours < ActiveRecord::Migration[7.0]
   def change
     create_table :tour_maven_tours do |t|
       t.string :label, null: false
-      t.jsonb :configuration, null: false, default: {}
+
+      if t.respond_to?(:jsonb)
+        t.jsonb :configuration, null: false, default: {}
+      else
+        t.json :configuration, null: false, default: {}
+      end
 
       t.timestamps
     end
